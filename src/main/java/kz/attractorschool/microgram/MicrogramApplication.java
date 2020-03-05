0494747b3e540e2cac3d702a709ec5dc979a947e
zlobbi <kadyrbek.mavlyanov@gmail.com>
2020-03-05 20:29:58 +0600

    add my files

--- src/main/java/kz/attractorschool/microgram/MicrogramApplication.java
+++ src/main/java/kz/attractorschool/microgram/MicrogramApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MicrogramApplication {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
         SpringApplication.run(MicrogramApplication.class, args);
     }
 
