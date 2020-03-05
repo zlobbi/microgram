182ef41eb3e50aa051eb75f5675f82a70f0e9993
zlobbi <kadyrbek.mavlyanov@gmail.com>
2020-03-05 19:54:28 +0600

    add all files

---
+++ src/test/java/kz/attractorschool/microgram/MicrogramApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.attractorschool.microgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrogramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
